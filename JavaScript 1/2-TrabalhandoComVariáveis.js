9e451a4def1f0d30fa24c84c740a58b6498d1b64
NathaliaLeticiaGuedesSoares <87998746+NathaliaLeticiaGuedesSoares@users.noreply.github.com>
2021-07-26 15:37:47 -0300

    Add files via upload
    Projetos dos cursos de Flexbox, HTM e CSS Parte 1,2,3 e 4; Javascript 1,2,3; Layouts Responsivos, Logica de programação 1 e 2.

---
+++ JavaScript 1/2-TrabalhandoComVariáveis.js
@@ -0,0 +1,7 @@
+console.log("Trabalhando com váriaveis");
+
+const idade = 15;
+const nome = "Nathy";
+
+console.log(idade);
+console.log(nome);
---
+++ JavaScript 1/5-Atribuição.js
@@ -0,0 +1,15 @@
+console.log("Trabalhando com atribuições de váriaveis");
+
+const idade = 15;
+const primeiroNome = "Nathy";
+const sobrenome = "Guedes"
+
+console.log(idade);
+console.log(primeiroNome, sobrenome);
+console.log(`Meu nome é: ${primeiroNome}`)
+
+let contador = 0;
+contador = contador +1;
+
+const nomeCompleto = primeiroNome+sobrenome;
+console.log(nomeCompleto);
